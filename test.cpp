ff801bf0783b38a758ea444327cc905620c6f69e
Alec Jacobson <alecjacobson@gmail.com>
2024-04-20 23:58:57 -0400

    working 2nd derivs!

--- test.cpp
+++ test.cpp
@@ -76,6 +76,7 @@ void call_and_time_all()
 int main() {
   // Some simple tests
   {
+    printf("# Some simple tests\n");
     using Scalar = double;
     Tape<Scalar> tape;
 
@@ -96,7 +97,68 @@ int main() {
   }
 
   // Performance benchmark 
+  printf("\n# Performance Benchmark with Eigen::Cholesky\n");
   call_and_time_all<1,16>();
+
+  // Second derivatives
+  {
+    printf("\n# Second derivatives\n");
+    Tape< double > tape_1;
+    Tape< Var<double> > tape_2;
+    Var<Var<double>> x(&tape_2, tape_2.push_scalar(), {&tape_1, tape_1.push_scalar(), 0.5});
+    Var<Var<double>> y(&tape_2, tape_2.push_scalar(), {&tape_1, tape_1.push_scalar(), 4.2});
+    //Var<Var<double>> z = x/y;
+    auto z = x * y + sin(x) + pow(x, y) + log(y) + exp(x) + cos(y);
+    auto dzd = z.grad();
+    printf("z = %g\n", z.getValue().getValue());
+    Var<double> dzdx = dzd[x.getIndex()];
+    printf("∂z/∂x = %g\n", dzdx.getValue());
+    Var<double> dzdy = dzd[y.getIndex()];
+    printf("∂z/∂y = %g\n", dzdy.getValue());
+    auto d2zdxd = dzdx.grad();
+    printf("∂²z/∂x² = %g\n", d2zdxd[x.getValue().getIndex()]);
+    printf("∂²z/∂x∂y = %g\n", d2zdxd[y.getValue().getIndex()]);
+    auto d2zdyd = dzdy.grad();
+    printf("∂²z/∂y∂x = %g\n", d2zdyd[x.getValue().getIndex()]);
+    printf("∂²z/∂y² = %g\n", d2zdyd[y.getValue().getIndex()]);
+  }
+
+  // Second derivatives with Eigen
+  {
+    printf("\n# Second derivatives with Eigen\n");
+    Tape< double > tape_1;
+    Tape< Var<double> > tape_2;
+    const int N = 2;
+    Eigen::Matrix<Var<Var<double>>, N, 1> x;
+    for (int i = 0; i < x.rows(); ++i) 
+    {
+      x(i) = Var<Var<double>>(&tape_2, tape_2.push_scalar(), {&tape_1, tape_1.push_scalar(), i+1});
+    }
+    Eigen::Matrix<Var<Var<double>>, N, N> A;
+    for (int i = 0; i < A.rows(); ++i) 
+    {
+      for (int j = 0; j < A.cols(); ++j) 
+      {
+        A(i,j) = x(i) + x(j);
+      }
+      A(i,i) = A(i,i)+A(i,i);
+    }
+    Eigen::Matrix<Var<Var<double>>, N, 1> b = A.llt().solve(x);
+    Var<Var<double>> y = b.array().square().sum();
+    auto dyd = y.grad();
+    for (int i = 0; i < N; ++i) 
+    {
+      printf("∂y/∂x(%d) = %g\n", i, dyd[x(i).getIndex()].getValue());
+    }
+    for (int i = 0; i < N; ++i) 
+    {
+      auto d2ydxi = dyd[x(i).getIndex()].grad();
+      for (int j = 0; j < N; ++j) 
+      {
+        printf("∂²y/∂x(%d)∂x(%d) = %g\n", i, j, d2ydxi[x(j).getValue().getIndex()]);
+      }
+    }
+  }
 
   return 0;
 }
