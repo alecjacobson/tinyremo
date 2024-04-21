36b5e48035e32af44ed2ad8ee82dcb372ad78f80
Alec Jacobson <alecjacobson@gmail.com>
2024-04-20 23:58:57 -0400

    touchup

--- tinyremo.h
+++ tinyremo.h
@@ -6,17 +6,20 @@
 #include <array>
 #include <cassert>
 #include <map>
+#include <unordered_map>
 
 // Adapted from the rust implementation at https://rufflewind.com/2016-12-30/reverse-mode-automatic-differentiation
 namespace tinyremo
 {
+  template<typename Key, typename Value> using index_map = std::unordered_map<Key,Value>;
+
   template<typename Scalar>
   struct Node 
   {
     std::array<Scalar, 2> weights;
     std::array<size_t, 2> deps;
   };
-  
+
   template<typename Scalar>
   class Tape 
   {
@@ -27,29 +30,29 @@ namespace tinyremo
       nodes.push_back({{Scalar(0),Scalar(0)}, {index, index}});
       return index;
     }
-  
+
     size_t push_unary(size_t dep0, Scalar weight0)
     {
       size_t index = nodes.size();
       nodes.push_back({{weight0, Scalar(0)}, {dep0, index}});
       return index;
     }
-  
+
     size_t push_binary(size_t dep0, Scalar weight0, size_t dep1, Scalar weight1)
     {
       size_t index = nodes.size();
       nodes.push_back({{weight0, weight1}, {dep0, dep1}});
       return index;
     }
-  
+
     size_t size() const { return nodes.size(); }
-  
+
     const Node<Scalar>& operator[](size_t index) const { return nodes[index]; }
-  
+
   private:
     std::vector<Node<Scalar>> nodes;
   };
-  
+
   template<typename Scalar>
   class Var {
   public:
@@ -63,15 +66,15 @@ namespace tinyremo
     // `Var<Scalar>(0)/Var<Scalar>(1)` kind of thing. So we need to support
     // those below (some are asserted to be unsupported currently).
     Var(Scalar value = Scalar(0)): tape_ptr(nullptr), index(0), value(value) { }
-    
+
     // Constructor for scalar types (e.g., int, double), but not for Var types.
     // SFINAE is used to enable this constructor only for scalar types.
     template<typename T, typename = typename std::enable_if<std::is_scalar<T>::value>::type>
     constexpr explicit Var(T v) : tape_ptr(nullptr), index(0), value(v) { }
-  
+
     // Construct for a tracked variable
     Var(Tape<Scalar>* tape_ptr, size_t index, const Scalar & value) : tape_ptr(tape_ptr), index(index), value(value) {}
-  
+
     Var operator+(const Var& other) const
     {
       assert(!tape_ptr || !other.tape_ptr || tape_ptr == other.tape_ptr);
@@ -80,12 +83,12 @@ namespace tinyremo
       if(other.tape_ptr) { return Var(other.tape_ptr, other.tape_ptr->push_unary(other.index, Scalar(1)), value + other.value); }
       return Var(value + other.value);
     }
-  
+
     Var operator-(const Var& other) const
     {
       return *this + (-other);
     }
-  
+
     Var operator*(const Var& other) const
     {
       assert(!tape_ptr || !other.tape_ptr || tape_ptr == other.tape_ptr);
@@ -94,13 +97,13 @@ namespace tinyremo
       if(other.tape_ptr) { return Var(other.tape_ptr, other.tape_ptr->push_unary(other.index, value), value * other.value); }
       return Var(value * other.value);
     }
-  
+
     Var operator/(const Var& other) const
     {
       assert(other.value != Scalar(0));
       return *this * other.invert();
     }
-  
+
     Var invert() const
     {
       if(tape_ptr)
@@ -111,7 +114,7 @@ namespace tinyremo
         return Var(Scalar(1) / value);
       }
     }
-  
+
     // Unary negation
     Var operator-() const
     {
@@ -123,20 +126,20 @@ namespace tinyremo
         return Var(-value);
       }
     }
-  
+
     // -= operator
     Var& operator-=(const Var& other) { *this = *this - other; return *this; }
     Var& operator+=(const Var& other) { *this = *this + other; return *this; }
     Var& operator*=(const Var& other) { *this = *this * other; return *this; }
     Var& operator/=(const Var& other) { *this = *this / other; return *this; }
-  
+
     // > operator
     bool operator>(const Var& other) const { return value > other.value; }
     bool operator>=(const Var& other) const { return value >= other.value; }
     bool operator<(const Var& other) const { return value < other.value; }
     bool operator<=(const Var& other) const { return value <= other.value; }
     bool operator==(const Var& other) const { return value == other.value; }
-  
+
     // Eigen will call `sqrt(x(i))` for `Eigen::Matrix<Var,…> x;`
     friend Var sqrt(const Var& v) {
       using std::sqrt;
@@ -214,12 +217,12 @@ namespace tinyremo
         }
       }
     }
-  
+
     std::vector<Scalar> grad() const
     {
       std::vector<Scalar> derivs(tape_ptr->size(), Scalar(0));
       derivs[index] = Scalar(1);
-  
+
       for (int i = tape_ptr->size() - 1; i >= 0; --i) {
         const auto& node = (*tape_ptr)[i];
         for (int j = 0; j < 2; ++j) {
@@ -228,20 +231,19 @@ namespace tinyremo
       }
       return derivs;
     }
-  
-    // Could use std::unordered_map if we decide we don't care about order here.
-    std::map<size_t, Scalar> sparse_grad() const 
+
+    index_map<size_t, Scalar> sparse_grad() const 
     {
-      std::map<size_t, Scalar> derivs;
+      index_map<size_t, Scalar> derivs;
       compute_sparse_grad(derivs, Scalar(1), index);
       return derivs;
     }
-  
+
     Scalar getValue() const { return value; }
     size_t getIndex() const { return index; }
-  
+
   private:
-    void compute_sparse_grad(std::map<size_t, Scalar>& derivs, Scalar grad_value, size_t idx) const
+    void compute_sparse_grad(index_map<size_t, Scalar>& derivs, Scalar grad_value, size_t idx) const
     {
       assert(idx < tape_ptr->size() && idx >= 0);
       if (derivs.find(idx) != derivs.end()) {
@@ -258,7 +260,7 @@ namespace tinyremo
         }
       }
     }
-  
+
     Tape<Scalar> * tape_ptr;
     size_t index;
     Scalar value;
